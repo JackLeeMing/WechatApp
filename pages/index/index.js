cad744ef33969caef3ea36a79514977845bdaea9
lijiakui <lijk@smartbow.net>
2018-05-11 15:24:59 +0800

    ..

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,33 +9,83 @@ const weatherMap = {
   'snow': '雪'
 };
 
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+};
+
 Page({
   data:{
-    wendu: 10,
-    tianqi: "--",
-    wbg:"sunny"
+    now:{
+      temp: 10,
+      wea: "--",
+      weather: "sunny",
+    },
+    forecast:[],
+  },
+  onPullDownRefresh(){
+    this.loadData(()=>{
+      wx.stopPullDownRefresh();
+    });
+  },
+
+  setNowWeather:(obj) =>{
+    if (obj) {
+      obj.weatherText = weatherMap[obj.weather];
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[obj.weather],
+      });
+    }
+  },
+
+  setForcastWeather: (hourlyWeather)=>{
+    if (hourlyWeather) {
+      let nowHour = new Date().getHours();
+      let i = 0;
+      hourlyWeather.forEach((item) => {
+        item.weatherText = weatherMap[item.weather];
+        if (i == 0) {
+          item.time = '现在';
+        } else {
+          item.time = (i + nowHour) % 24 + '时';
+        }
+        i += 3
+      });
+    }
+  },
+
+  loadData(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: "广州"
+      },
+      success: (res) => {
+        if (res && res.data && res.data.code == 200) {
+          console.log(res);
+          let result = res.data.result;
+          let nowWeather = result.now;
+          let hourlyWeather = result.forecast;
+          this.setNowWeather(nowWeather);
+          this.setForcastWeather(hourlyWeather);
+          this.setData({ now: nowWeather, forecast: hourlyWeather });
+        }
+      },
+      fail: (err) => {
+        console.error(err);
+      },
+      complete: ()=>{
+        callback && callback();
+      }
+    })
   },
   onLoad(){
     console.log("Hello world");
-    wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now',
-      data:{
-        city:"海南"
-      },
-      success:(res) => {
-        if(res && res.data && res.data.code == 200 ){
-          console.log(res);
-          let result = res.data.result;
-          let wendu = result.now.temp;
-          let weather = result.now.weather;
-          let tianqi = weatherMap[weather];
-          this.setData({ wendu: wendu, tianqi: tianqi, wbg:"lightrain"});
-          
-        }
-      },
-      fail:(err) => {
-        console.error(err);
-      }
-    })
+    this.loadData();
   }
 });
